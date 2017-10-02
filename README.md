fa3a6e53aa445495d258453d5a018978585c3d09
pvcresin <pvcresin0730@gmail.com>
2017-10-03 01:19:00 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# testRiotRoute
+
+riot route test
+
+riot-route https://github.com/riot/route
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+	context: path.resolve(__dirname, './src/js'),
+	entry: {
+		index: './index.js'
+	},
+	output: {
+		path: path.join(__dirname, 'dist/js/'),
+		filename: '[name].js'
+	},
+	module: {
+		loaders: [{
+			test: /\.tag$/,
+			enforce: 'pre',
+			exclude: /node_modules/,
+			loader: 'tag-pug-loader'
+		},{
+			test: /\.js|\.tag$/,
+			enforce: 'post',
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015-riot']
+				}
+			}
+		}]
+	},
+	resolve: {
+		extensions: ['.js', '.tag']
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			riot: 'riot'
+		})
+	]
+}
